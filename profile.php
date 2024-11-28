5b5d421f4161026465f8659dda653bea261f98d9
Alifia Oktavianingsih <if23.alifiaoktavianingsih@mhs.ubpkarawang.ac.id>
2024-11-28 20:27:36 +0700

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/cosmo/bootstrap.min.css"
+        integrity="sha384-5QFXyVb+lrCzdN228VS3HmzpiE7ZVwLQtkt+0d9W43LQMzz4HBnnqvVxKg6O+04d" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Selamat datang admin</h1>
+</body>
+</html>
